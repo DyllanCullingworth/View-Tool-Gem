1fc9262c242a6d7c897a04a9121bb08472262ca4
mmdyllan@gmail.com <mmdyllan@gmail.com>
2021-01-07 14:11:27 +0200

    initial commit and creation

---
+++ lib/dyllan_cullingworth_view_tool.rb
@@ -0,0 +1,7 @@
+require "dyllan_cullingworth_view_tool/version"
+require "dyllan_cullingworth_view_tool/renderer"
+
+module DyllanCullingworthViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dyllan_cullingworth_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DyllanCullingworthViewTool
+  VERSION = "0.1.0"
+end
